4898a1c4ec3334cc86d91e398a1263bcb8ede262
Rakesh <rakeshkumart2760@gmail.com>
2022-04-21 21:23:56 +0530

    react .net core

--- WebAPI/WebAPI/Models/Department.cs
+++ WebAPI/WebAPI/Models/Department.cs
@@ -7,7 +7,7 @@ namespace WebAPI.Models
 {
     public class Department
     {
-        public int DepartmentId { get; set; }
+        public int? DepartmentId { get; set; }
 
         public string DepartmentName { get; set; }
     }
--- WebAPI/WebAPI/Models/Employee.cs
+++ WebAPI/WebAPI/Models/Employee.cs
@@ -8,7 +8,7 @@ namespace WebAPI.Models
 {
     public class Employee
     {
-        public int EmployeeId { get; set; }
+        public int? EmployeeId { get; set; }
         public string EmployeeName { get; set; }
         public string Department { get; set; }
         public string DateOfJoining { get; set; }
